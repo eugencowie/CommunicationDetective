969bcad18ee607af158f4ea6223bb836425747e2
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-10-08 22:04:52 +0100

    Add functional multiplayer lobby

---
+++ Assets/Communication Detective/Scripts/Database.cs
@@ -0,0 +1,109 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using UnityEngine;
+
+public class Database
+{
+    private DatabaseReference m_root;
+
+    public Database()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://communication-detective.firebaseio.com");
+        m_root = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    /// <summary>
+    /// Pushes data to the database. This is an asynchronous operation.
+    /// </summary>
+    public void PushAsync(string path, string data)
+    {
+        m_root.Child(path).SetValueAsync(data);
+    }
+
+    /// <summary>
+    /// Pushes data to the database. This is an asynchronous operation which will call the
+    /// specified action on completion.
+    /// </summary>
+    public void PushAsync(string path, string data, Action<bool> returnSuccess)
+    {
+        m_root.Child(path).SetValueAsync(data).ContinueWith(t => {
+            returnSuccess(t.IsCompleted);
+        });
+    }
+
+    /// <summary>
+    /// Deletes data from the database. This is an asynchronous operation.
+    /// </summary>
+    public void DeleteAsync(string path)
+    {
+        PushAsync(path, null);
+    }
+
+    /// <summary>
+    /// Deletes data from the database. This is an asynchronous operation which will call
+    /// the specified action on completion.
+    /// </summary>
+    public void DeleteAsync(string path, Action<bool> returnSuccess)
+    {
+        PushAsync(path, null, returnSuccess);
+    }
+
+    /// <summary>
+    /// Pulls data from the database. This is an asynchronous operation which will call the
+    /// specified action on completion.
+    /// </summary>
+    public void PullAsync(string path, Action<string> returnResult)
+    {
+        m_root.Child(path).GetValueAsync().ContinueWith(t => {
+            returnResult(t.Result.Value.ToString());
+        });
+    }
+
+    /// <summary>
+    /// Checks if data exists in the database. This is an asynchronous operation which will call
+    /// the specified action on completion.
+    /// </summary>
+    public void ExistsAsync(string path, Action<bool> returnExists)
+    {
+        m_root.Child(path).GetValueAsync().ContinueWith(t => {
+            returnExists(t.Result.Exists);
+        });
+    }
+
+    public void RegisterListener(string path, EventHandler<ValueChangedEventArgs> listener)
+    {
+        FirebaseDatabase.DefaultInstance.GetReference(path).ValueChanged += listener;
+    }
+
+    public void DeregisterListener(string path, EventHandler<ValueChangedEventArgs> listener)
+    {
+        FirebaseDatabase.DefaultInstance.GetReference(path).ValueChanged -= listener;
+    }
+
+    /// <summary>
+    /// Tests to check if the database is working properly.
+    /// </summary>
+    public static void RunTests()
+    {
+        Database database = new Database();
+
+        database.ExistsAsync("test/does/not/exist", exists => {
+            Debug.Assert(exists == false);
+        });
+
+        database.PushAsync("test/data/key", "value", success => {
+            Debug.Assert(success == true);
+            database.ExistsAsync("test/data", exists => {
+                Debug.Assert(exists == true);
+                database.ExistsAsync("test/data/key", keyExists => {
+                    Debug.Assert(keyExists == true);
+                    database.PullAsync("test/data/key", result => {
+                        Debug.Assert(result == "value");
+                    });
+                });
+            });
+        });
+    }
+}
---
+++ Assets/Communication Detective/Scripts/MenuController.cs
@@ -0,0 +1,146 @@
+﻿using Firebase.Database;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [Tooltip("GameObject with a NetworkController component to handle network queries")]
+    public GameObject NetworkControllerObject;
+    private NetworkController NetworkController
+    {
+        get { return NetworkControllerObject.GetComponent<NetworkController>(); }
+    }
+
+    [Tooltip("GameObject with a Text component to display the generated room code")]
+    public GameObject CodeLabelObject;
+    private Text CodeLabel
+    {
+        get { return CodeLabelObject.GetComponent<Text>(); }
+    }
+
+    [Tooltip("GameObject with an InputField component to input the room code to join")]
+    public GameObject CodeFieldObject;
+    private InputField CodeField
+    {
+        get { return CodeFieldObject.GetComponent<InputField>(); }
+    }
+
+    [Tooltip("GameObject with a Text component to display the list of players in the room")]
+    public GameObject PlayersLabelObject;
+    private Text PlayersLabel
+    {
+        get { return PlayersLabelObject.GetComponent<Text>(); }
+    }
+
+    public GameObject StartPanel;
+    public GameObject JoinPanel;
+    public GameObject LobbyPanel;
+    public GameObject WaitPanel;
+
+    private void Start()
+    {
+        SwitchPanel(WaitPanel);
+
+        NetworkController.GetPlayerRoomAsync(room => {
+            if (string.IsNullOrEmpty(room)) SwitchPanel(StartPanel);
+            else {
+                CodeLabel.text = room;
+                RegisterOnPlayersChanged(room);
+                SwitchPanel(LobbyPanel);
+            }
+        });
+    }
+
+    public void JoinButtonPressed()
+    {
+        SwitchPanel(JoinPanel);
+    }
+
+    public void SubmitButtonPressed()
+    {
+        SwitchPanel(WaitPanel);
+
+        NetworkController.JoinRoomAsync(CodeField.text.ToUpper(), success => {
+            if (!success) {
+                CodeField.text = "";
+                SwitchPanel(JoinPanel);
+            } else {
+                CodeLabel.text = CodeField.text.ToUpper();
+                RegisterOnPlayersChanged(CodeLabel.text);
+                SwitchPanel(LobbyPanel);
+            }
+        });
+    }
+
+    public void CreateButtonPressed()
+    {
+        SwitchPanel(WaitPanel);
+
+        NetworkController.CreateCodeAsync(code => {
+            if (string.IsNullOrEmpty(code)) SwitchPanel(StartPanel);
+            else {
+                NetworkController.CreateRoomAsync(code, createSuccess => {
+                    if (!createSuccess) SwitchPanel(StartPanel);
+                    else {
+                        NetworkController.JoinRoomAsync(code, joinSuccess => {
+                            if (!joinSuccess) SwitchPanel(StartPanel);
+                            else {
+                                CodeLabel.text = code;
+                                RegisterOnPlayersChanged(code);
+                                SwitchPanel(LobbyPanel);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    public void LeaveButtonPressed()
+    {
+        SwitchPanel(WaitPanel);
+
+        NetworkController.LeaveRoomAsync(CodeLabel.text, success => {
+            if (success) {
+                DeregisterOnPlayersChanged(CodeLabel.text);
+                CodeLabel.text = "_____";
+                SwitchPanel(StartPanel);
+            }
+            else SwitchPanel(LobbyPanel);
+        });
+    }
+
+    private void SwitchPanel(GameObject panel)
+    {
+        // Disable all panels
+        foreach (var p in new GameObject[] { StartPanel, JoinPanel, LobbyPanel, WaitPanel })
+        {
+            p.SetActive(false);
+        }
+
+        // Enable specified panel
+        panel.SetActive(true);
+    }
+
+    private void RegisterOnPlayersChanged(string room)
+    {
+        string roomPlayersKey = string.Format("rooms/{0}/players", room);
+
+        NetworkController.RegisterListener(roomPlayersKey, OnPlayersChanged);
+    }
+
+    private void DeregisterOnPlayersChanged(string room)
+    {
+        string roomPlayersKey = string.Format("rooms/{0}/players", room);
+
+        NetworkController.DeregisterListener(roomPlayersKey, OnPlayersChanged);
+    }
+
+    private void OnPlayersChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.Snapshot.Exists)
+        {
+            PlayersLabel.text = args.Snapshot.Value.ToString().Replace(',', '\n');
+        }
+    }
+}
