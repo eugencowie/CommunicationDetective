b6229552c8e6bdc40f7fd0b1e2b346eb42214860
JustAnotherNullException <32852918+JustAnotherNullException@users.noreply.github.com>
2017-10-17 11:40:39 +0100

    Add room and camera - Add camera movement

---
+++ Assets/Communication Detective/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speedH = 0.10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        { 
+}
+            transform.Rotate(Vector3.up, speedH * Input.GetAxis("Mouse X"));
+
+    }
+}
