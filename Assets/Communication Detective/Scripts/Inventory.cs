87988db580d085f3c1369fd0df95b84484c14350
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-10-31 10:18:50 +0000

    Make inventory scrollable

--- Assets/Communication Detective/Scripts/Inventory.cs
+++ Assets/Communication Detective/Scripts/Inventory.cs
@@ -1,15 +1,47 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private GameObject ButtonContainer = null;
     [SerializeField] private GameObject Button = null;
 
     private List<GameObject> m_buttons = new List<GameObject>();
-    
-    private const int m_spacing = 280;
+    private const int m_spacing = 225;
+    private const float m_scrollSpeed = 2000;
+    private float m_scrollAmount = 0;
+
+    private Vector3 m_touchOrigin;
+    private bool m_isSwiping;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject())
+        {
+            m_touchOrigin = Input.mousePosition;
+            m_isSwiping = true;
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            m_isSwiping = false;
+        }
+
+        if (m_isSwiping)
+        {
+            Vector3 screenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition - m_touchOrigin);
+
+            float movement = screenPos.normalized.y * m_scrollSpeed * Time.deltaTime;
+            
+            if (Mathf.Abs(movement) > 20)
+            {
+                Scroll(movement);
+            }
+        }
+    }
 
     public void AddItem(ObjectHint item)
     {
@@ -20,7 +52,7 @@ public class Inventory : MonoBehaviour
             newButton.name = item.Name;
 
             // Set initial transform
-            newButton.transform.SetParent(transform);
+            newButton.transform.SetParent(ButtonContainer.transform);
             newButton.transform.localPosition = Button.transform.localPosition;
             newButton.transform.localRotation = Button.transform.localRotation;
             newButton.transform.localScale = Button.transform.localScale;
@@ -40,6 +72,10 @@ public class Inventory : MonoBehaviour
             // Activate button
             newButton.SetActive(true);
             m_buttons.Add(newButton);
+
+            // Scroll inventory to show the new item
+            float maxScrollAmount = Mathf.Max(0, (m_buttons.Count * m_spacing) - (m_spacing * 5));
+            ScrollTo(maxScrollAmount);
         }
     }
 
@@ -49,5 +85,30 @@ public class Inventory : MonoBehaviour
         {
             AddItem(item);
         }
+    }
+
+    public void ScrollUpButtonPressed()
+    {
+        Scroll(-m_spacing);
+    }
+
+    public void ScrollDownButtonPressed()
+    {
+        Scroll(m_spacing);
+    }
+
+    private void Scroll(float movement)
+    {
+        ScrollTo(m_scrollAmount + movement);
+    }
+
+    private void ScrollTo(float position)
+    {
+        m_scrollAmount = position;
+
+        float maxScrollAMount = Mathf.Max(0, (m_buttons.Count * m_spacing) - (m_spacing * 5));
+        m_scrollAmount = Mathf.Clamp(m_scrollAmount, 0, maxScrollAMount);
+
+        ButtonContainer.transform.localPosition = new Vector3(0, m_scrollAmount, 0);
     }
 }
