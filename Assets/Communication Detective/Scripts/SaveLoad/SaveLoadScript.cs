a2cc9b21c7c31c53cd933993a7f5ef98731a1be0
winterfressh <bencekovacs00@gmail.com>
2017-11-16 12:38:58 +0000

    Added saving and loading inventory on button press

---
+++ Assets/Communication Detective/Scripts/SaveLoad/SaveLoadScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveLoadScript{
+
+    //public static List<ObjectHintData> savedGames = new List<ObjectHintData>();
+	
+    public static void Save()
+    {
+        //savedGames.Add(ObjectHintData.current);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, StaticInventory.Hints);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            StaticInventory.Hints = (List<ObjectHintData>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+}
